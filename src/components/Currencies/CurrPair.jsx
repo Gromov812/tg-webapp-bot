94b801fc9709dbd9ed6d3760f5170d6baa98b4c1
Nikita Gromov <nikita.gromoff@gmail.com>
2023-05-23 12:05:47 +0300

    push

--- src/components/Currencies/CurrPair.jsx
+++ src/components/Currencies/CurrPair.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CurrPair = ({name, value}) => {
     return (
         <div className={'currPair'}>
-            <p>{name} - {value}</p>
+            <p>{name} >> {value}</p>
         </div>
     );
 };
