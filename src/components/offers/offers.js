ea38e6d778bf55d1dd6022cea06321fa497bf3ed
Nikita <nikita.gromoff@gmail.com>
2023-08-16 19:06:48 +0300

    qq

--- src/components/offers/offers.js
+++ src/components/offers/offers.js
@@ -1,7 +1,9 @@
-export default offers = [
+let offers = [
     {   title: 123,
         img: 'https://avatars.mds.yandex.net/get-altay/4365916/2a0000017914e032d1c7a0a1d03586577cd9/XXXL',
         delay: 1,
+        text: 'Ставка 0% первые 14 дней',
+        bg: 'warning',
         money: 'до 30 000 рублей', 
         percent: 'от 0%',
         period: 'от 7 до 30 дней',
@@ -9,4 +11,6 @@ export default offers = [
         document: 'паспорт РФ',
         recive: 'на карту'
     }
-]
+];
+
+export default offers;
